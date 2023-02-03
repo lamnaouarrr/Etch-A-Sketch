b76fb11863ed43af9cd752c957837097aa2bd31a
Ayoub <ayoublamnaouar1@gmail.com>
2023-02-03 03:32:04 +0100

    first-version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Etch-A-Sketch
+
+video source:
+
+Author      One Idea LLC
+Category    Abstract
+Duration    0:13
+Size        72.34 MB
+Dimensions  1920 x 1080
+File Type   MP4
+link        https://isorepublic.com/video/abstract-colored-waves/
---
+++ script.js
@@ -0,0 +1,63 @@
+let isMouseDown = false;
+let color = "black";
+
+function createBoard(size){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+
+    for (let i = 0; i < amount; i++) {
+        let square =  document.createElement("div");
+        square.style.backgroundColor = "white";
+        square.style.border = "1px solid #f5f6fa";
+        square.addEventListener("mousedown",function(){
+            isMouseDown = true;
+            colorSquare(square);
+        });
+        square.addEventListener("mousemove",function(){
+            if(isMouseDown){
+                colorSquare(square);    
+            };
+        });
+        square.addEventListener("mouseup",function(){
+            isMouseDown = false;
+        });
+
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100 ) {
+        createBoard(input);
+        document.querySelector('.error').textContent = "";
+    }
+    else{
+        document.querySelector('.error').textContent = "⚠️ Please enter a number between 2 and 100!";
+    }
+}
+
+function changeColor(choice){
+    color = choice;
+}
+
+function colorSquare(square){
+    if(color === 'random'){
+        square.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    else
+        square.style.backgroundColor = color;
+}
+
+function resetBoard(){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "white");
+}
+
+
+createBoard(16);
